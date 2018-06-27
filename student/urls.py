088a820423218d59c6c36198e4ddec1e67e68cb8
Diwaker Ghimire <idwaker@gmail.com>
2018-06-27 07:57:23 +0545

    updated student routes

--- student/urls.py
+++ student/urls.py
@@ -1,8 +1,15 @@
 
 from django.urls import path
 
-from .views import get_students
+from .views import (
+    get_all_student,
+    get_student,
+    add_student,
+    update_student,
+    delete_student
+)
 
 urlpatterns = [
-    path("", get_students)
+    path("", get_all_student),
+    path("<int:student_id>", get_student),
 ]
--- student/views.py
+++ student/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from .models import Student
 
 # Create your views here.
-def get_students(request):
+def get_all_student(request):
     students = Student.objects.all()
     print(students)
     html = """<table><tr><td>Fullname</td><td>Address</td></tr>"""
@@ -14,3 +14,29 @@ def get_students(request):
         html += "<td>" + student.address + "</td></tr>"
     html += "</table>"
     return HttpResponse(html)
+
+
+def add_student(request):
+    return HttpResponse("Not Implemented")
+
+
+def get_student(request, student_id):
+    try:
+        student = Student.objects.get(pk=student_id)
+    except Student.DoesNotExist:
+        return HttpResponse("Resource Not found.")
+    # print(Student.objects.filter(id=student_id).query)
+    html = f"""
+    <h2>Name: {student.fullname}</h2>
+    <h3>Date Of Birth: {student.dob}</h3>
+    <h3>Address: {student.address}</h3>
+    """
+    return HttpResponse(html)
+
+
+def update_student(request):
+    return HttpResponse("Not Implemented")
+
+
+def delete_student(request):
+    return HttpResponse("Not Implemented")
