f684d7ccaec5a7524a0eff703c897ada81743973
Diwaker Ghimire <idwaker@gmail.com>
2018-07-02 08:57:35 +0545

    updates

--- student/admin.py
+++ student/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import Student
+from .models import Student, StudentInfo
 
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ("fullname", "address", "dob")
-    search_fields = ("address",)
-    list_filter = ("dob",)
+    list_display = ("fullname",)
+    # search_fields = ("address",)
+    # list_filter = ("dob",)
+
+
+class StudentInfoAdmin(admin.ModelAdmin):
+    list_display = ("student", "crn", "dob")
 
 admin.site.register(Student, StudentAdmin)
+admin.site.register(StudentInfo, StudentInfoAdmin)
--- student/models.py
+++ student/models.py
@@ -2,11 +2,28 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
+    crn = models.IntegerField(unique=True)
+    level = models.IntegerField()
+    section = models.CharField(max_length=1)
     fullname = models.CharField(max_length=128)
-    address = models.TextField()
-    dob = models.DateField(verbose_name="Date Of Birth")
 
     def __repr__(self):
         return self.fullname
 
     __str__ = __repr__
+
+
+class StudentInfo(models.Model):
+    address = models.TextField()
+    dob = models.DateField(verbose_name="Date Of Birth")
+
+    student = models.OneToOneField(
+        Student,
+        on_delete=models.CASCADE,
+        verbose_name="student"
+    )
+
+    @property
+    def crn(self):
+        # student_info.crn
+        return self.student.crn
